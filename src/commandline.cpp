d2d18fb9488d8633abb28b598c02530fce4d437c
animetosho <animetosho@users.noreply.github.com>
2023-05-24 19:15:03 +1000

    Add source project versions to `-VV` output Ref #3

--- src/commandline.cpp
+++ src/commandline.cpp
@@ -222,7 +222,7 @@ bool CommandLine::ReadArgs(int argc, const char * const *argv)
       else if (argv[0] == string("-VV"))
       {
 	showversion();
-	cout << endl;
+	cout << "A par2cmdline version 0.8.1~0.8.2 fork, using a ParPar v0.4.0 processing backend" << endl << endl;
 	banner();
 	return true;
       }
