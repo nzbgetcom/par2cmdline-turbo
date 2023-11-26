dfcec3c8e0b1fd00521f8d6e19bb3d1385814ccf
animetosho <animetosho@users.noreply.github.com>
2023-11-26 19:03:02 +1000

    Remove ParPar version from description (I'll likely forget to update it)

--- src/commandline.cpp
+++ src/commandline.cpp
@@ -210,7 +210,7 @@ bool CommandLine::ReadArgs(int argc, const char * const *argv)
       else if (argv[0] == string("-VV"))
       {
 	showversion();
-	cout << "A par2cmdline version 0.8.1~0.8.2 fork, using a ParPar v0.4.0 processing backend" << endl << endl;
+	cout << "A par2cmdline version 0.8.1~0.8.2 fork, using a ParPar processing backend" << endl << endl;
 	banner();
 	return true;
       }
