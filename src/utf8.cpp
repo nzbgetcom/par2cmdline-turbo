abe9c9b43ba98d4963b6ec037ee2f1ce2d05494d
Denis <146707790+dnzbk@users.noreply.github.com>
2024-10-07 10:23:55 +0300

    Unicode and long-path support

--- src/utf8.cpp
+++ src/utf8.cpp
@@ -17,21 +17,18 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+
 #include "libpar2internal.h"
 
 #include <cstring>
 #include <iostream>
 #include <exception>
 
-#include "utf8.h"
+#include <par2/utf8.h>
 
-namespace utf8
+namespace Par2
 {
-  const int MAX_ARGS = 128;
-  const size_t MAX_DIR_PATH = 248;
-  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> UTF8_CONVERTER;
-
-  std::wstring Utf8ToWide(const std::string& str)
+  std::optional<std::wstring> Utf8ToWide(const std::string& str)
   {
     if (str.empty())
       return L"";
@@ -63,7 +60,7 @@ namespace utf8
     }
   }
 
-  std::string WideToUtf8(const std::wstring& str)
+  std::optional<std::string> WideToUtf8(const std::wstring& str)
   {
     if (str.empty())
       return "";
@@ -75,8 +72,30 @@ namespace utf8
     catch (const std::exception& e)
     {
       std::cerr << "Failed to convert wide to UTF-8 string: " << e.what() << std::endl;
-      return "";
+      return std::nullopt;
     }
+  }
+
+  std::string Latin1ToUtf8(const std::string& latin1Str)
+  {
+    if (latin1Str.empty()) return "";
+
+    std::string utf8Str;
+    utf8Str.reserve(latin1Str.length() * 2);
+
+    for (unsigned char ch : latin1Str)
+    {
+      if (ch < 128)
+      {
+        utf8Str.push_back(ch);
+      }
+      else
+      {
+        utf8Str.push_back(0xc2 + (ch > 0xbf));
+        utf8Str.push_back((ch & 0x3f) + 0x80);
+      }
+    }
+    return utf8Str;
   }
 
   WideToUtf8ArgsAdapter::WideToUtf8ArgsAdapter(int argc, wchar_t* wargv[]) noexcept(false)
@@ -109,8 +128,14 @@ namespace utf8
         continue;
       }
 
-      std::string arg = WideToUtf8(wargv[i]);
-      size_t size = arg.size() + 1;
+      auto arg = WideToUtf8(wargv[i]);
+      if (!arg.has_value())
+      {
+        std::wcerr << L"Failed to convert " << wargv[i] << L" to UTF-8 string. Skipping" << std::endl;
+        continue;
+      }
+
+      size_t size = arg->size() + 1;
       m_argv[i] = new char[size];
       std::memcpy(m_argv[i], arg.c_str(), size);
     }
