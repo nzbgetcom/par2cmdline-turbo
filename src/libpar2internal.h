189b596d444a4680f710df1ccc08d4969265210f
dnzbk <denis@nzbget.com>
2025-02-12 18:13:42 +0300

    Add: UNC paths support

--- src/libpar2internal.h
+++ src/libpar2internal.h
@@ -203,6 +203,7 @@ char *strchr(), *strrchr();
 
 #include <ctype.h>
 #include <iomanip>
+#include <codecvt>
 
 #include <cassert>
 
@@ -246,6 +247,7 @@ using namespace std;
 #include "par1fileformat.h"
 #include "par1repairersourcefile.h"
 #include "par1repairer.h"
+#include "utf8.h"
 
 // Heap checking
 #ifdef _MSC_VER
