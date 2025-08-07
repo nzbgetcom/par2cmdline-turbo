nzbgetcom/par2cmdline-turbo

commit bbacdb7636cf1a687cfc66fac0d7cbe40ccdc028

Add: NZBGet specific changes

src/diskfile.cpp
@@ -20,6 +20,13 @@
 
 #include "libpar2internal.h"
 
+#include <ostream>
+#include <string>
+#include <list>
+
+using namespace Par2;
+using namespace std;
+
 #ifdef _MSC_VER
 #ifdef _DEBUG
 #undef THIS_FILE
@@ -71,7 +78,7 @@ bool DiskFile::CreateParentDirectory(string _pathname)
       string::npos != (where = _pathname.find_last_of('\\')))
   {
     string path = filename.substr(0, where);
-    wstring wpath = utf8::Utf8ToWide(path);
+    wstring wpath = Utf8ToWide(path);
 
     struct _stat st;
     if (_wstat(wpath.c_str(), &st) == 0)
@@ -105,7 +112,7 @@ bool DiskFile::Create(string _filename, u64 _filesize)
     return false;
 
   // Create the file
-  wstring wfilename = utf8::Utf8ToWide(_filename);
+  wstring wfilename = Utf8ToWide(_filename);
   hFile = ::CreateFile(wfilename.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_NEW, 0, NULL);
   if (hFile == INVALID_HANDLE_VALUE)
   {
@@ -228,7 +235,7 @@ bool DiskFile::Open(const string &_filename, u64 _filesize)
   filename = _filename;
   filesize = _filesize;
 
-  wstring wfilename = utf8::Utf8ToWide(_filename);
+  wstring wfilename = Utf8ToWide(_filename);
   hFile = ::CreateFile(wfilename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
   if (hFile == INVALID_HANDLE_VALUE)
   {
@@ -321,7 +328,7 @@ void DiskFile::Close(void)
 
 string DiskFile::GetCanonicalPathname(string filename)
 {
-  std::wstring wfilename = utf8::Utf8ToWide(filename);
+  std::wstring wfilename = Utf8ToWide(filename);
   auto wfullname = make_unique<wchar_t[]>(MAX_PATH); 
 
   size_t length = GetFullPathName(wfilename.data(), MAX_PATH, wfullname.get(), nullptr);
@@ -333,7 +340,7 @@ string DiskFile::GetCanonicalPathname(string filename)
   wfullname[0] = towupper(wfullname[0]);
   replace(wfullname.get(), wfullname.get() + length, L'/', L'\\');
 
-  return utf8::WideToUtf8(wfullname.get());
+  return WideToUtf8(wfullname.get());
 }
 
 std::unique_ptr< list<string> > DiskFile::FindFiles(string path, string wildcard, bool recursive)
@@ -346,7 +353,7 @@ std::unique_ptr< list<string> > DiskFile::FindFiles(string path, string wildcard
   }
   list<string> *matches = new list<string>;
 
-  wstring wwildcard = utf8::Utf8ToWide(path + wildcard);
+  wstring wwildcard = Utf8ToWide(path + wildcard);
   WIN32_FIND_DATA fd;
   HANDLE h = ::FindFirstFile(wwildcard.c_str(), &fd);
   if (h != INVALID_HANDLE_VALUE)
@@ -355,7 +362,7 @@ std::unique_ptr< list<string> > DiskFile::FindFiles(string path, string wildcard
     {
       if (0 == (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
       {
-        matches->push_back(path + utf8::WideToUtf8(fd.cFileName));
+        matches->push_back(path + WideToUtf8(fd.cFileName));
       }
       else if (recursive == true)
       {
@@ -365,7 +372,7 @@ std::unique_ptr< list<string> > DiskFile::FindFiles(string path, string wildcard
 
         string nwwildcard="*";
 	std::unique_ptr< list<string> > dirmatches(
-						 DiskFile::FindFiles(path + utf8::WideToUtf8(fd.cFileName), nwwildcard, true)
+						 DiskFile::FindFiles(path + WideToUtf8(fd.cFileName), nwwildcard, true)
 						 );
 
         // sort both lists before merge
@@ -383,7 +390,7 @@ std::unique_ptr< list<string> > DiskFile::FindFiles(string path, string wildcard
 
 u64 DiskFile::GetFileSize(string filename)
 {
-  wstring wfilename = utf8::Utf8ToWide(filename);
+  wstring wfilename = Utf8ToWide(filename);
   struct _stati64 st;
   if ((0 == _wstati64(wfilename.c_str(), &st)) && (0 != (st.st_mode & S_IFREG)))
   {
@@ -397,9 +404,9 @@ u64 DiskFile::GetFileSize(string filename)
 
 bool DiskFile::FileExists(string filename)
 {
-  wstring wfilename = utf8::Utf8ToWide(filename);
-  struct _stati64 st;
-  return ((0 == _wstati64(wfilename.c_str(), &st)) && (0 != (st.st_mode & _S_IFREG))); 
+  wstring wfilename = Utf8ToWide(filename);
+  struct _stat st;
+  return ((0 == _wstat(wfilename.c_str(), &st)) && (0 != (st.st_mode & _S_IFREG)));
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -938,7 +945,7 @@ bool DiskFile::Delete(void)
 //  }
 //}
 
-void DiskFile::SplitFilename(string filename, string &path, string &name)
+void DiskFile::SplitFilename(const string& filename, string &path, string &name)
 {
   string::size_type where;
 
@@ -1018,8 +1025,8 @@ bool DiskFile::Rename(string _filename)
 {
   assert(hFile == INVALID_HANDLE_VALUE);
 
-  std::wstring wfilename = utf8::Utf8ToWide(filename);
-  std::wstring _wfilename = utf8::Utf8ToWide(_filename);
+  std::wstring wfilename = Utf8ToWide(filename);
+  std::wstring _wfilename = Utf8ToWide(_filename);
 
   if (::_wrename(wfilename.c_str(), _wfilename.c_str()) == 0)
   {

src/libpar2internal.h
@@ -200,14 +200,22 @@ char *strchr(), *strrchr();
 #include <list>
 #include <map>
 #include <algorithm>
-
+#include <memory>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_set>
+#include <mutex>
+#include <atomic>
+#include <future>
+#include <functional>
+#include <exception>
+#include <fstream>
 #include <ctype.h>
 #include <iomanip>
 #include <codecvt>
 
 #include <cassert>
-
-using namespace std;
 
 #ifdef offsetof
 #undef offsetof
@@ -215,42 +223,34 @@ using namespace std;
 #define offsetof(TYPE, MEMBER) ((size_t) ((char*)(&((TYPE *)1)->MEMBER) - (char*)1))
 
 // par2cmdline includes
-#include "libpar2.h"
-
-#include "letype.h"
-
-#include "galois.h"
-#include "crc.h"
-#include "md5.h"
-#include "par2fileformat.h"
-
-#include "diskfile.h"
-#include "datablock.h"
-
-#include "criticalpacket.h"
-#include "par2creatorsourcefile.h"
-
-#include "mainpacket.h"
-#include "creatorpacket.h"
-#include "descriptionpacket.h"
-#include "verificationpacket.h"
-#include "recoverypacket.h"
-
-#include "par2repairersourcefile.h"
-
-#include "filechecksummer.h"
-#include "verificationhashtable.h"
-
-#include "par2creator.h"
-#include "par2repairer.h"
-
-#include "par1fileformat.h"
-#include "par1repairersourcefile.h"
-#include "par1repairer.h"
-
-#ifdef _WIN32
-#include "utf8.h"
-#endif
+#include <par2/commandline.h>
+#include <par2/crc.h>
+#include <par2/creatorpacket.h>
+#include <par2/criticalpacket.h>
+#include <par2/datablock.h>
+#include <par2/descriptionpacket.h>
+#include <par2/diskfile.h>
+#include <par2/filechecksummer.h>
+#include <par2/foreach_parallel.h>
+#include <par2/galois.h>
+#include <par2/hasher.h>
+#include <par2/letype.h>
+#include <par2/libpar2.h>
+#include <par2/mainpacket.h>
+#include <par2/md5.h>
+#include <par2/par1fileformat.h>
+#include <par2/par1repairer.h>
+#include <par2/par1repairersourcefile.h>
+#include <par2/par2creator.h>
+#include <par2/par2creatorsourcefile.h>
+#include <par2/par2fileformat.h>
+#include <par2/par2repairer.h>
+#include <par2/par2repairersourcefile.h>
+#include <par2/recoverypacket.h>
+#include <par2/reedsolomon.h>
+#include <par2/utf8.h>
+#include <par2/verificationhashtable.h>
+#include <par2/verificationpacket.h>
 
 // Heap checking
 #ifdef _MSC_VER

commit ce900f5309709c31f2d51af83891377bda90b0b3

Large files (size >32 bit) treated as if they do not exist (#5)
* Fix: large file support on Windows

Fix regression introduced in bbacdb7 by using a stat variant that
supports both 64-bit sizes and 64-bit times (regardless of
_USE_32BIT_TIME_T).

* always support 64-bit times/sizes on Windows

Consistently use _(w)stat64().

src/diskfile.cpp
@@ -81,8 +81,8 @@ bool DiskFile::CreateParentDirectory(string _pathname)
     auto wpath = Utf8ToWide(path);
     if (!wpath.has_value()) return false;
 
-    struct _stat st;
-    if (_wstat(wpath->c_str(), &st) == 0)
+    struct _stat64 st;
+    if (_wstat64(wpath->c_str(), &st) == 0)
       return true; // let the caller deal with non-directories
 
     if (!DiskFile::CreateParentDirectory(path))
@@ -404,8 +404,8 @@ u64 DiskFile::GetFileSize(string filename)
   auto wfilename = Utf8ToWide(filename);
   if (!wfilename.has_value()) return 0;
 
-  struct _stati64 st;
-  if ((0 == _wstati64(wfilename->c_str(), &st)) && (0 != (st.st_mode & S_IFREG)))
+  struct _stat64 st;
+  if ((0 == _wstat64(wfilename->c_str(), &st)) && (0 != (st.st_mode & S_IFREG)))
   {
     return st.st_size;
   }
@@ -420,8 +420,8 @@ bool DiskFile::FileExists(string filename)
   auto wfilename = Utf8ToWide(filename);
   if (!wfilename.has_value()) return false;
 
-  struct _stat st;
-  return ((0 == _wstat(wfilename->c_str(), &st)) && (0 != (st.st_mode & _S_IFREG)));
+  struct _stat64 st;
+  return ((0 == _wstat64(wfilename->c_str(), &st)) && (0 != (st.st_mode & _S_IFREG)));
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////

src/libpar2internal.h
@@ -41,7 +41,7 @@
 #define sprintf  sprintf_s
 #define stricmp  _stricmp
 #define unlink   _unlink
-#define stat _stat
+#define stat _stat64
 
 #define __LITTLE_ENDIAN 1234
 #define __BIG_ENDIAN    4321