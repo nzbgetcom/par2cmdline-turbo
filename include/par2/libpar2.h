6e6712eccd526c65c66e007d3716346382916acf
Denis <146707790+dnzbk@users.noreply.github.com>
2026-03-23 08:29:27 +0300

    Fix: Ensure build with GCC 15+ by explicitly including <cstdint> (#6)

--- include/par2/libpar2.h
+++ include/par2/libpar2.h
@@ -23,6 +23,7 @@
 #include <ostream>
 #include <vector>
 #include <string>
+#include <cstdint>
 
 namespace Par2
 {
@@ -53,8 +54,6 @@ typedef signed __int64   i64;
 #  include <inttypes.h>
 #endif
 
-#if HAVE_STDINT_H
-#  include <stdint.h>
 typedef uint8_t            u8;
 typedef int8_t             i8;
 typedef uint16_t           u16;
@@ -63,16 +62,6 @@ typedef uint32_t           u32;
 typedef int32_t            i32;
 typedef uint64_t           u64;
 typedef int64_t            i64;
-#else
-typedef unsigned char      u8;
-typedef signed char        i8;
-typedef unsigned short     u16;
-typedef signed short       i16;
-typedef unsigned int       u32;
-typedef signed int         i32;
-typedef unsigned long long u64;
-typedef signed long long   i64;
-#endif
 
 #else // HAVE_CONFIG_H
 
