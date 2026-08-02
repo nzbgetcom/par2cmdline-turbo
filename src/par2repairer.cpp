9f06ba855dc5b0e094e9dbc2314d4838ae60d0ea
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-08-02 10:30:44 +0100

    Fix long names in SigDone

--- src/par2repairer.cpp
+++ src/par2repairer.cpp
@@ -1667,7 +1667,7 @@ bool Par2Repairer::ScanDataFile(DiskFile                *diskfile,    // [in]
   }
   else
   {
-    shortname = std::move(name);
+    shortname = name;
   }
 
   // Create the checksummer for the file and start reading from it
